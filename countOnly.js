d611d67a1f0c5d7b801f7c3f755d45f330a105ba
Vanessa Y <vyeung.wt@gmail.com>
2021-02-03 23:47:40 +0000

    code linted

--- countOnly.js
+++ countOnly.js
@@ -14,17 +14,17 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-    for (const item of allItems) {
-      if (itemsToCount[item]) {
-        if (results[item]) {
-          results[item] += 1;
-        } else {
-          results[item] = 1;
-        }
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
       }
     }
-  return results; 
-}
+  }
+  return results;
+};
 
 //TEST CASE
 
