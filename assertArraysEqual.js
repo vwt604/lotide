846d8171444fb6247a7abefcdbb03a997dc6b95e
Vanessa Y <vyeung.wt@gmail.com>
2021-02-03 05:15:57 +0000

    added solution

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 🛑 🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 5], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, "3"], [1, 2, 3]));
