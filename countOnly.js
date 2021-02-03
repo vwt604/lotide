d861248bf78b3ff15681cd7d3dd9f28054658d4e
Vanessa Y <vyeung.wt@gmail.com>
2021-02-03 23:37:07 +0000

    added solution

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+// TEST/ASSERTION FUNCTIONS
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} ==! ${expected}`);
+  }
+};
+
+//THE FUNCTION: returns a report on all the strings found in the input array, and their respective counts. Therefore it will need to return an object that can represent the stats.
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+    for (const item of allItems) {
+      if (itemsToCount[item]) {
+        if (results[item]) {
+          results[item] += 1;
+        } else {
+          results[item] = 1;
+        }
+      }
+    }
+  return results; 
+}
+
+//TEST CASE
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], 1);
