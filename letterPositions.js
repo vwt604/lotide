1ad35489c1435f92450fa3d7250075b52d2b15da
Vanessa Y <vyeung.wt@gmail.com>
2021-02-04 03:38:50 +0000

    added solution

---
+++ letterPositions.js
@@ -0,0 +1,44 @@
+//TEST/ASSERTION FUNCTIONS
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ ✅ ✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 🛑 🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+//THE FUNCTION: returns all the indices (zero-based positions) in the string where each character is found
+
+const letterPositions = function(sentence) {
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+
+
+//TEST CASE
+
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("goodbye").o, [1,2]);
