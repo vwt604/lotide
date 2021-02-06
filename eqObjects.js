f387ee2b63bec2005b1f78fc2ad591755c1766ab
Vanessa Y <vyeung.wt@gmail.com>
2021-02-06 03:29:28 +0000

    added linted solution

--- eqObjects.js
+++ eqObjects.js
@@ -1,31 +1,42 @@
-//FUNCTION: takes in an object and a value. Scans the object and return the first key which contains the given value. If no key is found, return undefined.
+//FUNCTION:  takes in two arrays and returns true or false, based on a perfect match.
+// const eqArrays = function(array1, array2) {
+//   if (array1.length === array2.length) {
+//     for (let i = 0; i < array1.length; i++) {
+//       if (array1[i] !== array2[i]) {
+//         return false;
+//       }
+//     }
+//   } else {
+//     return false;
+//   }
+//   return true;
+// };
 
-const eqObjects = function(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
+//FUNCTION: takes in two objects and returns true or false, based on a perfect match.
+
+const eqObjects = function(object1, object2) {      // function taking in two objects
+  let array1 = Object.keys(object1);                //array of object1 keys
+  let array2 = Object.keys(object2);               //array of object2 keys
+
+  if (array1.length !== array2.length) {           //checks length match
     return false;
+  
   } else {
-    for (let key in object1) {
-      if (Array.isArray(object1[key])) {
-        if (eqArrays(object1[key], object2[key]) === false) {
-          return false;
-        }
-      } else if (typeof object1[key] === 'object') {
-        if (eqObjects(object1[key], object2[key]) === false) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]) {
+    for (let element of array1) {                   //loops through elements of array1 if array lengths match
+      if (object1[element] !== object2[element]) { //checks element match
         return false;
       }
     }
-  } 
-  return true;
+  }
+  return true;                                    //return true if all match
 };
+
 
 //TEST CASE
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+console.log(eqObjects(ab, ba));
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+console.log(eqObjects(ab, abc))
